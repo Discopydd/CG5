51abc3891412dd9981ae4b1f960ddd8666908615
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2025-05-04 17:17:28 +0900

    00_01

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -35,7 +35,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
 		assert(false);
 	}
-
 	// バイナリをもとに生成
 	ID3D12RootSignature* rootSignature = nullptr;
 	hr = dxCommon->GetDevice()->CreateRootSignature(
